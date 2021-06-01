337b6917f19a6bc44bd2bc0c7af630c6248c55cf
Martin <61944410+martinjoshevski@users.noreply.github.com>
2021-06-01 19:02:23 +0200

    Title updated

--- src/Banner.js
+++ src/Banner.js
@@ -32,16 +32,15 @@ function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            `This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description`,
-            150
-          )}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
       <div className="banner--fadeBottom" />
