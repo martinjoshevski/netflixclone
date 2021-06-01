54c644c27dd78cca83000438cc2d6249c9813305
Martin <61944410+martinjoshevski@users.noreply.github.com>
2021-06-01 18:44:51 +0200

    Starting the project

---
+++ src/Banner.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
+
+function truncate(string, n) {
+  return string?.length > n ? string.substr(0, n - 1) + " ..." : string;
+}
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
+    // return () => {};
+  }, []);
+  console.log(movie);
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(
+            `This is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test DescriptionThis is a test Description`,
+            150
+          )}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-TbtYOrO_rbOk0PAN7f1BST7h_0WXIl0",
+  authDomain: "netflix-v1-a8b91.firebaseapp.com",
+  projectId: "netflix-v1-a8b91",
+  storageBucket: "netflix-v1-a8b91.appspot.com",
+  messagingSenderId: "749080414994",
+  appId: "1:749080414994:web:58a20fbea93b1e46f941cb",
+};
