ceb8d4f9663ff6053a3aa1bcb0cd6f85c8357526
Martin <61944410+martinjoshevski@users.noreply.github.com>
2021-08-31 18:43:28 +0200

    Updated

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,8 +1,12 @@
+import { loadStripe } from "@stripe/stripe-js";
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 import db from "../firebase";
 import "./PlansScreen.css";
 function PlansScreen() {
   const [products, setProducts] = useState([]);
+  const user = useSelector(selectUser);
   useEffect(() => {
     db.collection("products")
       .where("active", "==", true)
@@ -22,7 +26,49 @@ function PlansScreen() {
         setProducts(products);
       });
   }, []);
-  return <div className="plansScreen"></div>;
+  const loadCheckout = async (priceId) => {
+    const docRef = await db
+      .collection("customers")
+      .doc(user.uid)
+      .collection("checkout_session")
+      .add({
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      });
+    docRef.onSnapshot(async (snap) => {
+      const { error, sessionId } = snap.data();
+      if (error) {
+        alert(`An error occured: ${error.message}`);
+      }
+      if (sessionId) {
+        const stripe = await loadStripe(
+          "pk_test_51JUY1tLbVwR5ig2D5oRSQoCNajmPGpbpsXjYOa1G9c4LydX3NttVeVuXB5dEAEauaE9zBO6Br03vSN1JGtYD1uFh00AIUP9UCX"
+        );
+        stripe.redirectToCheckout({ sessionId });
+      }
+    });
+  };
+  return (
+    <div className="plansScreen">
+      {Object.entries(products).map(([productId, productData]) => {
+        return (
+          <div className="plansScreen__plan">
+            <div className="plansScreen__info">
+              <h5>{productData.name}</h5>
+              <h6>{productData.description}</h6>
+            </div>
+            <button
+              className="plansScreen__button"
+              onClick={() => loadCheckout(productData.prices.priceId)}
+            >
+              Subscribe
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default PlansScreen;
